e8b03c1e52c1040694ce3b005935566cad7937ed
rakesh <rakeshcpr011@gmail.com>
2026-05-13 09:27:45 +0530

    d

--- drift_monitoring/app_v1.py
+++ drift_monitoring/app_v1.py
@@ -2,8 +2,9 @@ import streamlit as st
 import pandas as pd
 import os
 import glob
-from evidently.report import Report
-from evidently.metric_preset import DataDriftPreset, DataQualityPreset
+# ✅ YE LAGAO
+from evidently import Report
+from evidently.presets import DataDriftPreset, DataQualityPreset
 from datetime import datetime, timedelta
 
 # Local paths
